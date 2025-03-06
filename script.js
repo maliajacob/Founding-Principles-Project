f9e5840a85ad1b18c381e89286c2027cb1f242bf
maliatjacob <malia.jacob0717@stu.mtec.edu>
2025-03-05 21:51:19 -0700

    alert button and person page mods

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.getElementById("scroll-button").addEventListener("click", function() {
     document.getElementById("target-heading").scrollIntoView({ behavior: 'smooth' });
 });
+
+// Get all elements with class="closebtn"
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+// Loop through all close buttons
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.opacity = "0";
+    setTimeout(function() { div.style.display = "none"; }, 600);
+  }
+}
